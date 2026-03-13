dfee3b3cae097d51985450b63853f0a7a0d1595a
Guilherme Nunes <guilherme.augd@gmail.com>
2026-03-13 10:47:22 -0300

    feat: add data class with data types

---
+++ app/src/main/java/br/com/fiap/fiapapp/domain/Usuario.kt
@@ -0,0 +1,11 @@
+package br.com.fiap.fiapapp.domain
+
+data class Usuario (
+    val nome: String = "",
+    val email: String = "",
+    val senha: String = "",
+    val temaEscuro: Boolean = false
+)
+
+
+
